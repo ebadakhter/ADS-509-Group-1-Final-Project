23c0c081549f69f6803054a4ffbfb62d8f0ef60b
Kayhe93 <112589922+Kayhe93@users.noreply.github.com>
2024-02-23 17:27:18 -0800

    Update recipe_app.py
    update more ingredients input

--- flask_app/recipe_app.py
+++ flask_app/recipe_app.py
@@ -11,13 +11,25 @@ app.config['SECRET_KEY'] = 'd2c44137ec36c4b1e41fcd54565313b4'
 all_recipes = pd.read_csv('all_recipes.csv')
 
 
-@app.route("/", methods=['GET', 'POST'])
 def home():
     form = RecipesSearchForm()
     if form.validate_on_submit():
-        result_df = all_recipes.loc[all_recipes['Ingredients'].str.contains(form.ingredients_search.data)]
-        result = result_df.to_json(orient="records")
+        ingredients_list = [ingredient.strip().lower() for ingredient in form.ingredients_search.data.split(',')]
+
+        # Initialize an empty list to store matching recipes
+        matching_recipes = []
+
+        # Iterate through each recipe
+        for _, recipe in all_recipes.iterrows():
+            recipe_ingredients = recipe['Ingredients'].lower()
+
+            # Check if all specified ingredients are in the recipe
+            if all(ingredient in recipe_ingredients for ingredient in ingredients_list):
+                matching_recipes.append(recipe.to_dict())
+
+        result = json.dumps(matching_recipes)
         return render_template('home.html', form=form, recipes=json.loads(result))
+
     return render_template('home.html', form=form)
 
 
