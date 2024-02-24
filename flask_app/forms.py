9940539e5dc06e4ea2b4f37e64116a2430b8a517
Kayhe93 <112589922+Kayhe93@users.noreply.github.com>
2024-02-23 17:28:36 -0800

    Update forms.py
    add warning that input ingredients with commas

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class RecipesSearchForm(FlaskForm):
-    ingredients_search = StringField('Ingredients Search(1 Ingredient Limit)',
+    ingredients_search = StringField('Ingredients Search(Separate multiple ingredients with commas)',
                          validators=[DataRequired(), Length(min=2, max=20)],
                                      render_kw={'style': 'font-size: 18px;'})
     search = SubmitField('Search')
